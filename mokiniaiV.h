c50ff33bbcb33d88c5d469f7b4f1a81bf32be582
Rapolas Kairys <rapolas.kairys@gmail.com>
2022-05-30 00:52:08 +0300

    sukurta klase

--- mokiniaiV.h
+++ mokiniaiV.h
@@ -1,6 +1,18 @@
 #pragma once
 #include <vector>
 #include <string>
+
+class Studentas
+{
+  private:
+    std::string name = "";
+    std::string surename = "";
+    std::vector<double> hw;
+    int egzam;
+    int size = 0;
+    double mediana = 0;
+}
+
 struct mokinys
 {
     std::string name = "", surename = "";
